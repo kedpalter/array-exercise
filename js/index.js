8ac4edcbde71b7f7ee58ac0920c4df99dea11fce
Kepler <tandat6444@gmail.com>
2025-06-07 14:32:18 +0700

    js2

--- js/index.js
+++ js/index.js
@@ -28,6 +28,48 @@ let sumPositive = function (arrInput) {
     }
     return sum;
 }
+// Đếm số dương
+let countPositive = function (arrInput) {
+    let count = 0;
+    for (i in arrInput) {
+        if (arrInput[i] > 0) {
+            count += 1;
+        }
+    }
+    return count;
+}
+// Tìm số nhỏ nhất
+let minOfArr = function (arrInput) {
+    let min = arrInput[0];
+    for (i in arrInput) {
+        if (arrInput[i] < min) {
+            min = arrInput[i];
+        }
+    }
+    return min;
+}
+// Tìm số dương nhỏ nhất
+let minOfPositive = function (arrInput) {
+    // let arrPos = [];
+    let minPos = 1;
+
+    for (value of arrInput) {
+        if (value > 0) {
+            if (value < minPos) {
+                minPos = value;
+            } 
+        } 
+        // console.log(arrPos);
+    }
+    return minPos;
+
+}
+
+// Đổi vị trí
+// Sắp xếp tăng dần 
+// Tìm số nguyên tố đầu tiên
+// Đếm số nguyên
+// So sánh số lượng số âm và dương
 
 // Bài 1
 document.querySelector('#btnBai1').onclick = function () {
@@ -36,8 +78,12 @@ document.querySelector('#btnBai1').onclick = function () {
     document.getElementById('ketQua1').innerHTML = `Tổng số dương trong chuỗi: <strong>${tong}</strong>.`
 }
 
-
 // Bài 2
+document.querySelector('#btnBai2').onclick = function () {
+    let dem = countPositive(arrDauVao);
+    document.getElementById('ketQua2').innerHTML = `Trong chuỗi có <strong>${dem}</strong> số dương.`
+}
+
 // Bài 3
 // Bài 4
 // Bài 5
